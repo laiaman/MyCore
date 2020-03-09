233b7a134ed99b5b81fd7e86dfc7bc6be9be2fe7
laiaman <chenjianlin2399@163.com>
2020-03-09 14:31:37 +0800

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            IEnumerable<Student> students = _studentRepository.GetAllStudents();
+            
+            return View(students);
+         
+        }
+        [Route("Home/Details/{id?}")]
+        public IActionResult Details(int? id)
+        {
+
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Student = _studentRepository.GetStudent(id??1),
+                Title = "详细信息"
+            };
+
+           return View(homeDetailsViewModel);
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)//配置
+        {
+            
+            services.AddMvc();
+
+            services.AddSingleton<IStudentRepository, MockStudentRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)//管理运行池的管道
+        {
+            if (env.IsDevelopment())
+            {
+                //DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+                //developerExceptionPageOptions.SourceCodeLineCount = 5;
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvc();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute("default", "{controller=home}/{action=index}/{id?}");
+            //});
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //app.Run(async (context) =>//终端型中间键
+            //{
+            //    //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            //    //   var config = _configuration["MyKey"];
+            //    //  throw new Exception("管道异常");
+            //    // context.Response.ContentType = "text/plain;charset=utf-8";
+            //    await context.Response.WriteAsync("hello world ");
+
+            //});
+
+
+
+            //app.Use(async (context,next) =>
+            //{
+            //    //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            //    //   var config = _configuration["MyKey"];
+            //    logger.LogInformation("1传入请求");
+            //    context.Response.ContentType = "text/plain;charset=utf-8";
+            //    await context.Response.WriteAsync("this is first Hello World!");
+            //    await next();
+            //    logger.LogInformation("1传出请求");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            //    //   var config = _configuration["MyKey"];
+            //    logger.LogInformation("2传入请求");
+            //    await context.Response.WriteAsync("this is first Hello World!");
+            //    await next();
+            //    logger.LogInformation("2传出请求");
+            //});
+
+
+
+
+
+            //DefaultFilesOptions defaultFiles = new DefaultFilesOptions();
+            //defaultFiles.DefaultFileNames.Clear();
+            //defaultFiles.DefaultFileNames.Add("lonsid.html");
+
+
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("lonsid.html");
+
+            //   app.UseFileServer();
+
+
+            //app.UseWelcomePage();
+            //app.UseDefaultFiles();
+
+            ////添加静态文件
+            //app.UseStaticFiles();
+
+
+
+
+        }
+    }
+}
